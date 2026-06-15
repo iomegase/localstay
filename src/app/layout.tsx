9f3da3c0317871d91b172354bac3821fc5c5e94e
devillers <davidevillers@gmail.com>
2026-06-15 23:42:07 +0200

    oprngraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,13 @@ export const metadata: Metadata = {
     title: SITE.defaultTitle,
     description: SITE.defaultDescription,
     url: '/',
+    images: [{ url: '/imageOpenGraph.png', width: 1200, height: 630, alt: SITE.defaultTitle }],
   },
   twitter: {
     card: 'summary_large_image',
     title: SITE.defaultTitle,
     description: SITE.defaultDescription,
+    images: ['/imageOpenGraph.png'],
   },
   appleWebApp: {
     capable: true,
